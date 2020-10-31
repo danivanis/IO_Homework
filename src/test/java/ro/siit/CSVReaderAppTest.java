b3976bd7e4fb7633be7c100e44a78d741bd27be7
dan.ivanis@pm.me <G1tHU8C0Nt+1>
2020-11-01 01:07:29 +0200

    Third commit

--- src/test/java/ro/siit/CSVReaderAppTest.java
+++ src/test/java/ro/siit/CSVReaderAppTest.java
@@ -5,7 +5,7 @@ import org.junit.Test;
 
 import java.io.*;
 
-public class CSVReaderAppV2Test extends TestCase {
+public class CSVReaderAppTest extends TestCase {
 
     @Test
     public void testGetFile(){
